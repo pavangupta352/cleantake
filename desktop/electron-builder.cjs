43ad902829714aeb02e9773b65abdb0cbc0b042f
Pavan Gupta <pavan.gupta.352@gmail.com>
2026-09-08 07:02:54 +0530

    Omit the unused Windows elevation updater helper

--- desktop/electron-builder.cjs
+++ desktop/electron-builder.cjs
@@ -28,7 +28,7 @@ module.exports = {
   },
   dmg: { title: "CleanTake ${version}" },
   win: { target: [{ target: "nsis", arch: [process.arch] }] },
-  nsis: { oneClick: true, perMachine: false, runAfterFinish: true, createDesktopShortcut: true, createStartMenuShortcut: true, deleteAppDataOnUninstall: false },
+  nsis: { oneClick: true, perMachine: false, packElevateHelper: false, runAfterFinish: true, createDesktopShortcut: true, createStartMenuShortcut: true, deleteAppDataOnUninstall: false },
   linux: { executableName: "cleantake", category: "AudioVideo;Audio;AudioVideoEditing", target: [{ target: "deb", arch: [process.arch] }, { target: "tar.xz", arch: [process.arch] }], synopsis: "Recover dialogue from your backup recordings" },
   deb: { packageName: "cleantake", depends: ["libgtk-3-0", "libnotify4", "libnss3", "libxss1", "libxtst6", "xdg-utils", "libatspi2.0-0", "libuuid1", "libsecret-1-0", "libasound2 | libasound2t64", "libgbm1", "libdrm2"] },
 };
